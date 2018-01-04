7572e42308ff8140563d4a6b78a5e1b022983d11
Wacław Wojcieszko <wojciech.waclawik@gmail.com>
2018-01-05 00:20:17 +0100

    DEV

--- src/org/wojcieszko/Main.java
+++ src/org/wojcieszko/Main.java
@@ -1,17 +1,8 @@
 package org.wojcieszko;
 
-import org.wojcieszko.homework.homework02.*;
-
 public class Main {
 
     public static void main(String[] args) {
-        Person kolo = new Person("90123098765", "Roger", "Odrzutowiec");
-//        kolo.setFirstName("Roger");
-//        kolo.setSurName("Odrzutowiec");
-//        kolo.setPesel("90123098765");
-
-        System.out.println(kolo.getSurName());
-
+	// write your code here
     }
 }
-
