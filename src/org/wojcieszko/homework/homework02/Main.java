c862b898f8d03dcc6b3b0f7036720846deaccfa5
Wacław Wojcieszko <wojciech.waclawik@gmail.com>
2018-01-04 23:09:29 +0100

    DEV

--- src/org/wojcieszko/homework/homework02/Main.java
+++ src/org/wojcieszko/homework/homework02/Main.java
@@ -1,8 +1,22 @@
 package org.wojcieszko.homework.homework02;
 
+import org.wojcieszko.homework.homework02.Person;
+import org.wojcieszko.homework.homework02.Student;
+import org.wojcieszko.homework.homework02.Employee;
+import org.wojcieszko.homework.homework02.Instructor;
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Person kolo = new Person();
+        kolo.setFirstName("Roger");
+        kolo.setSurName("Odrzutowiec");
+        kolo.setPesel("90123098765");
+
+        String x = kolo.getSurName();
+
+        System.out.println(x);
+
     }
 }
--- src/org/wojcieszko/homework/homework02/Person.java
+++ src/org/wojcieszko/homework/homework02/Person.java
@@ -1,4 +1,33 @@
 package org.wojcieszko.homework.homework02;
 
 public class Person {
+
+    private String pesel;
+    private String firstName;
+    private String surName;
+
+    public String getPesel(String pesel) {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public String getFirstName(String firstName) {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
 }
