1a93a842864b84a710b247f999b975c1b72dbc7d
Wacław Wojcieszko <wojciech.waclawik@gmail.com>
2018-03-15 00:24:20 +0100

    Homework06
    zaległa praca domowa w homework.homework06 klasa Reader

--- src/org/wojcieszko/lesson/lesson06/Lesson.java
+++ src/org/wojcieszko/lesson/lesson06/Lesson.java
@@ -3,7 +3,6 @@ package org.wojcieszko.lesson.lesson06;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
