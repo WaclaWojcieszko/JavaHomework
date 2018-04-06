18d5b77c73cf8361914d5e8339a682a55b127e26
Wacław Wojcieszko <wojciech.waclawik@gmail.com>
2018-04-06 21:35:57 +0200

    Homework08 Troszkę bezczelnie to zrobione może coś ciekawszego wymyślę jeszcze.
    Taaaaki durny błąd w pierwszym podejściu, aż mi wstyd...

--- src/org/wojcieszko/homework/homework08/SecondValue.java
+++ src/org/wojcieszko/homework/homework08/SecondValue.java
@@ -1,24 +1,32 @@
 package org.wojcieszko.homework.homework08;
 
+import org.junit.Test;
+
 public class SecondValue {
-    public static void main(String[] args) {
 
+    private int[] table;
 
-        int[] table1 = {1, 2, 3, 4};
-        int[] table2 = {1, 3, 2, 3};
+    public int[] getTable() {
+        return table;
+    }
+
+    public SecondValue(int[] table) {
+        this.table = table;
+    }
+
+    public int leastValue() {
+
         int[] value = new int[2];
 
-
-        for (int t : table2) {
+        for (int t : table) {
             if (t >= value[1]) {
                 value[0] = value[1];
                 value[1] = t;
+            } else if (t >= value[0]) {
+                value[0] = t;
             }
         }
-
-
-        System.out.println(value[0]);
-
+        return value[0];
     }
 
 }
