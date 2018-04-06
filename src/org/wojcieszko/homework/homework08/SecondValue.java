bcae264ca8ab34cb3bc468f6626100b972dca335
Wacław Wojcieszko <wojciech.waclawik@gmail.com>
2018-04-06 18:27:27 +0200

    Homework08 Troszkę bezczelnie to zrobione może coś ciekawszego wymyślę jeszcze.

---
+++ src/org/wojcieszko/homework/homework08/SecondValue.java
@@ -0,0 +1,27 @@
+package org.wojcieszko.homework.homework08;
+
+
+import com.sun.javafx.UnmodifiableArrayList;
+
+public class SecondValue {
+    public static void main(String[] args) {
+
+
+        int[] table1 = {1, 2, 3, 4};
+        int[] table2 = {1, 3, 2, 3};
+        int[] value = new int[2];
+
+
+            for (int t :table2) {
+                if (t >= value[1]){
+                    value[0] = value[1];
+                    value[1] = t;
+                }
+            }
+
+
+        System.out.println(value[0]);
+
+    }
+
+}
