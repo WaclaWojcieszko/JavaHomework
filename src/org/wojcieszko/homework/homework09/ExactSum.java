63cdc6bc666c67540df2001f0d4da38faaa87874
Wacław Wojcieszko <wojciech.waclawik@gmail.com>
2018-04-15 00:15:04 +0200

    homework09

---
+++ src/org/wojcieszko/homework/homework09/ExactSum.java
@@ -0,0 +1,61 @@
+package org.wojcieszko.homework.homework09;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ExactSum {
+
+    public Integer sum(List<Integer> elements){
+        Integer sum = 0;
+        for (Integer x : elements) {
+            sum += x;
+        }
+        return sum;
+    }
+
+
+    public void chooser(List<Integer> items, int sum){
+        chooserHelper(new ArrayList<>(), items, sum);
+    }
+    public void chooserHelper(List<Integer> constItems, List<Integer> items, int sum) {
+
+        List<Integer> constItemsCopy = new ArrayList<>(constItems);
+        List<Integer> itemsCopy = new ArrayList<>(items);
+
+        if (items.size() == 0) {
+            return;
+        }
+
+
+        for (int index = 0; index < itemsCopy.size(); ++index) {
+
+
+            items = new ArrayList<>(itemsCopy);
+            constItems = new ArrayList<>(constItemsCopy);
+
+
+            if (sum(constItems) == 6) {
+                System.out.println(constItems.toString());
+            }
+
+            constItems.add(items.get(index));
+            items.remove(index);
+
+            chooserHelper(constItems, items, sum);
+        }
+
+
+    }
+
+
+
+    @Test
+    public void permChooserTest() {
+        List<Integer> items = new ArrayList<>(Arrays.asList(1, 3, 3, 5));
+        int sum = 6;
+        chooser(items, sum);
+    }
+}
